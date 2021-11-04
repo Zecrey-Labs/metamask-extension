971840021193edd22283acb9825363fba7d69ac1
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2021-11-05 00:27:58 +0400

    Disable edit if there's simulation error
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
+++ ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
@@ -318,6 +318,11 @@ export default class ConfirmTransactionBase extends Component {
     } = this.props;
     const { t } = this.context;
 
+    const { valid } = this.getErrorKey();
+    const isDisabled = () => {
+      return this.state.confirmAnyways ? false : !valid;
+    };
+
     const hasSimulationError = Boolean(this.props.txData.simulationFails);
 
     const renderTotalMaxAmount = () => {
@@ -522,6 +527,7 @@ export default class ConfirmTransactionBase extends Component {
     return (
       <div className="confirm-page-container-content__details">
         <TransactionDetail
+          disabled={isDisabled()}
           onEdit={() => this.handleEditGas()}
           rows={[
             renderGasDetailsItem(),
